268395f3ce98ae28777d087f03bc4fea9b048f89
user <user@70802-PC04>
2020-09-21 17:03:27 +0800

    如何從字典查字並抓下來

--- src/main/java/lendle/courses/network/clientserverbasics/DictClient.java
+++ src/main/java/lendle/courses/network/clientserverbasics/DictClient.java
@@ -24,23 +24,23 @@ public class DictClient {
      */
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
-        Socket socket = new Socket("dict.org", 2628); 
-        socket.setSoTimeout(15000);
+        Socket socket = new Socket("dict.org", 2628); //將伺服器連結
+        socket.setSoTimeout(15000);//可以不打
         //hint: 從 socket 取得 OutputStream
         OutputStream out = socket.getOutputStream();
         ////////////////////////////////////////////
         Writer writer = new OutputStreamWriter(out, "UTF-8");
         //hint: 輸出 DEFINE wn gold\r\n
-        writer.write("DEFINE wn gold\r\n");
+        writer.write("DEFINE wn gold\r\n");// \r\n是enter鍵的意思
         /////////////////////////////
-        writer.flush();
+        writer.flush(); // 緩衝
         //hint: 從socket 取得 InputStream
         InputStream in = socket.getInputStream();
         ////////////////////////////////
         BufferedReader reader = new BufferedReader( 
                 new InputStreamReader(in, "UTF-8"));//一行一行讀
         for (String line = reader.readLine(); !line.equals("."); line = reader.readLine()) {
-            //hint: 將 line 變數輸出到終端機 
+            //hint: 將 line 變數輸出到終端機  //讀到.時換下一行
             System.out.println(line);
             //////////////////////
         }
